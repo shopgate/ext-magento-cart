3679408752142113000ab94e2597b17e69353b7f
Rainer Skistims <rainer.skistims@shopgate.com>
2019-02-01 14:18:32 +0100

    MAGENTO-2349 add items: map properties (shopgate) to options (magento)

--- extension/models/requestProducts/simpleProduct.js
+++ extension/models/requestProducts/simpleProduct.js
@@ -2,13 +2,23 @@ class SimpleProduct {
   constructor (productId, quantity) {
     this.productId = productId
     this.quantity = quantity
+    this.properties = {}
+  }
+
+  /**
+   * @param {string} key
+   * @param {string} value
+   */
+  addProperties (key, value) {
+    this.properties[key] = value
   }
 
   toJSON () {
     return {
       product: {
         'product_id': this.productId,
-        'qty': this.quantity
+        'qty': this.quantity,
+        'options': this.properties
       }
     }
   }
